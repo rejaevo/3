a8fa1ca48d0735c9fb6427f4bd5011d85edba2cf
BuildTools <unconfigured@null.spigotmc.org>
2021-05-04 17:20:04 +0300

    Updates

--- precache-manifest.7ae6047a2f4a55c4c4ca7f29261a76ca.js
+++ precache-manifest.7ae6047a2f4a55c4c4ca7f29261a76ca.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "91cb5f18cc90ed547d9e1dc698fcdf6d",
+    "revision": "1af20d9fcae2746fc8ab6e67316360dc",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "dda45209fd30ff577b0e",
+    "revision": "476bb2b40475232f9f0a",
     "url": "./static/css/main.981f7211.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.3f51ff30.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dda45209fd30ff577b0e",
-    "url": "./static/js/main.c8409ebf.chunk.js"
+    "revision": "476bb2b40475232f9f0a",
+    "url": "./static/js/main.c770ef90.chunk.js"
   },
   {
     "revision": "cf45432d129a816c05f6",
